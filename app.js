0b67bcf99ff2dd559e0f93094711f242f84bccaf
milegonzalez <mmilano1809@gmail.com>
2018-07-20 13:51:05 -0400

    html, index, and app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  //attach an event listener to the buttons
+  //create a function stuf for read/write/delet/
+      // research local storage
+
+
+//jquere selection template.
+  $().on('click' , function(){
+
+  });
+
+  $().on('click' , function(){
+
+  });    
+});
